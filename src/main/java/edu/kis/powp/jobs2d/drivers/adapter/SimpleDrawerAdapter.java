ec556d6fb82dec1adb970a1efbdcdb7413cf0ee1
neyluu <75064225+neyluu@users.noreply.github.com>
2026-03-11 17:55:17 +0100

    Removed unnecessary inheritance

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/SimpleDrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/SimpleDrawerAdapter.java
@@ -9,7 +9,7 @@ import edu.kis.powp.jobs2d.features.DrawerFeature;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class SimpleDrawerAdapter extends DrawPanelController implements Job2dDriver {
+public class SimpleDrawerAdapter implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
 	public SimpleDrawerAdapter() {

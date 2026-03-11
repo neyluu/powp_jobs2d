98a7472134bedee05d63b84f0066a9dd5ccb1fc1
neyluu <75064225+neyluu@users.noreply.github.com>
2026-03-11 13:17:47 +0100

    Fixed drawing shape in wrong window

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/EnvelopeAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/EnvelopeAdapter.java
@@ -4,6 +4,7 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
@@ -29,7 +30,8 @@ public class EnvelopeAdapter extends DrawPanelController implements Job2dDriver 
 		this.startX = x;
 		this.startY = y;
 
-		drawLine(line);
+		DrawPanelController drawPanelController = DrawerFeature.getDrawerController();
+		drawPanelController.drawLine(line);
 	}
 
 	@Override
